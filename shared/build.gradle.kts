88b7fb31e58bf959183d8eb92b5832f4a955a55c
Mofe Ejegi <mofeejegi@gmail.com>
2023-11-16 16:44:56 +0100

    Included voyager dependencies

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -30,12 +30,20 @@ kotlin {
                 implementation(compose.material)
                 @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
                 implementation(compose.components.resources)
+
                 implementation(libs.ktor.client.core)
                 implementation(libs.ktor.client.content.negotiation)
                 implementation(libs.ktor.serialization.kotlinx.json)
+
                 implementation(libs.kotlinx.serialization.json)
                 implementation(libs.kotlinx.coroutines.core)
                 implementation(libs.kotlinx.datetime)
+
+                implementation(libs.voyager.navigator)
+                implementation(libs.voyager.bottomSheetNavigator)
+                implementation(libs.voyager.koin)
+                implementation(libs.voyager.transitions)
+                implementation(libs.voyager.tabNavigator)
             }
         }
         val androidMain by getting {
