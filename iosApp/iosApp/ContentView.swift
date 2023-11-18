9f250963ecaa44ac8d64a91717c64cb2d2580b3b
Mofe Ejegi <mofeejegi@gmail.com>
2023-11-18 22:57:59 +0100

    Minor cleanup

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -3,7 +3,6 @@ import SwiftUI
 import shared
 
 struct ComposeView: UIViewControllerRepresentable {
-
     func makeUIViewController(context: Context) -> UIViewController {
         Main_iosKt.MainViewController()
     }
@@ -12,7 +11,6 @@ struct ComposeView: UIViewControllerRepresentable {
 }
 
 struct ContentView: View {
-
     var body: some View {
         ComposeView()
                 .ignoresSafeArea(.all, edges: .bottom) // Compose has own keyboard handler
