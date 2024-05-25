b1ac3003692fa1c4cc27328df33d5849f66eef22
Mofe Ejegi <mofeejegi@gmail.com>
2024-05-25 11:27:11 +0100

    Post-update cleanup

--- composeApp/src/iosMain/kotlin/com/studyround/app/platform/ui/KeyEvent.ios.kt
+++ composeApp/src/iosMain/kotlin/com/studyround/app/platform/ui/KeyEvent.ios.kt
@@ -1,6 +1,6 @@
 package com.studyround.app.platform.ui
 
 import androidx.compose.ui.input.key.KeyEvent
-import org.jetbrains.skiko.SkikoKey
 
-actual fun KeyEvent.isBackspaceKeyEvent(): Boolean = nativeKeyEvent.key == SkikoKey.KEY_BACKSPACE
+// TODO: Remove this eventually
+actual fun KeyEvent.isBackspaceKeyEvent(): Boolean = false //nativeKeyEvent.key == SkikoKey.KEY_BACKSPACE
