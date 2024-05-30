8e6f7fb16d932d94d08b15bc3c4d1ba775e6750c
Mofe Ejegi <mofeejegi@gmail.com>
2024-05-30 12:06:53 +0100

    Dummy categorised courses

--- composeApp/src/commonMain/kotlin/com/studyround/app/ui/features/dashboard/home/DashboardHomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/studyround/app/ui/features/dashboard/home/DashboardHomeScreen.kt
@@ -4,18 +4,16 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
-import com.studyround.app.ui.composables.common.StudyRoundTextLogo
+import com.studyround.app.data.model.remote.dto.Category
+import com.studyround.app.data.model.remote.dto.Course
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import studyround.composeapp.generated.resources.Res
 import studyround.composeapp.generated.resources.home
-import studyround.composeapp.generated.resources.ic_error
 import studyround.composeapp.generated.resources.ic_home
-import studyround.composeapp.generated.resources.other
 
 class DashboardHomeScreen: Tab {
 
@@ -23,9 +21,52 @@ class DashboardHomeScreen: Tab {
     override fun Content() {
         Box(
             modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center,
         ) {
-            StudyRoundTextLogo()
+            CategorisedCoursesContent(categories = listOf(
+                Category(
+                    1, "One",
+                    listOf(
+                        Course(1, title = "A course", rating = 3.77f, formattedPrice = "Free", imageUrl = "https://backend.studyround.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcWdCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--02144408fb9e7ff4ca98a8af7ef457d649664e60/image.jpeg"),
+                        Course(1, title = "A course", rating = 3.77f, formattedPrice = "Free", imageUrl = "https://backend.studyround.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcWdCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--02144408fb9e7ff4ca98a8af7ef457d649664e60/image.jpeg"),
+                        Course(1, title = "A course", rating = 3.77f, formattedPrice = "Free", imageUrl = "https://backend.studyround.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcWdCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--02144408fb9e7ff4ca98a8af7ef457d649664e60/image.jpeg"),
+                        Course(1, title = "A course", rating = 3.77f, formattedPrice = "Free", imageUrl = "https://backend.studyround.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcWdCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--02144408fb9e7ff4ca98a8af7ef457d649664e60/image.jpeg"),
+                        Course(1, title = "A course", rating = 3.77f, formattedPrice = "Free", imageUrl = "https://backend.studyround.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcWdCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--02144408fb9e7ff4ca98a8af7ef457d649664e60/image.jpeg"),
+                    )
+                ),
+
+                Category(
+                    1, "Two",
+                    listOf(
+                        Course(1, title = "A course", rating = 3.77f, formattedPrice = "Free", imageUrl = "https://backend.studyround.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcWdCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--02144408fb9e7ff4ca98a8af7ef457d649664e60/image.jpeg"),
+                        Course(1, title = "A course", rating = 3.77f, formattedPrice = "Free", imageUrl = "https://backend.studyround.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcWdCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--02144408fb9e7ff4ca98a8af7ef457d649664e60/image.jpeg"),
+                        Course(1, title = "A course", rating = 3.77f, formattedPrice = "Free", imageUrl = "https://backend.studyround.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcWdCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--02144408fb9e7ff4ca98a8af7ef457d649664e60/image.jpeg"),
+                        Course(1, title = "A course", rating = 3.77f, formattedPrice = "Free", imageUrl = "https://backend.studyround.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcWdCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--02144408fb9e7ff4ca98a8af7ef457d649664e60/image.jpeg"),
+                        Course(1, title = "A course", rating = 3.77f, formattedPrice = "Free", imageUrl = "https://backend.studyround.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcWdCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--02144408fb9e7ff4ca98a8af7ef457d649664e60/image.jpeg"),
+                    )
+                ),
+
+                Category(
+                    1, "Three",
+                    listOf(
+                        Course(1, title = "A course", rating = 3.77f, formattedPrice = "Free", imageUrl = "https://backend.studyround.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcWdCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--02144408fb9e7ff4ca98a8af7ef457d649664e60/image.jpeg"),
+                        Course(1, title = "A course", rating = 3.77f, formattedPrice = "Free", imageUrl = "https://backend.studyround.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcWdCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--02144408fb9e7ff4ca98a8af7ef457d649664e60/image.jpeg"),
+                        Course(1, title = "A course", rating = 3.77f, formattedPrice = "Free", imageUrl = "https://backend.studyround.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcWdCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--02144408fb9e7ff4ca98a8af7ef457d649664e60/image.jpeg"),
+                        Course(1, title = "A course", rating = 3.77f, formattedPrice = "Free", imageUrl = "https://backend.studyround.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcWdCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--02144408fb9e7ff4ca98a8af7ef457d649664e60/image.jpeg"),
+                        Course(1, title = "A course", rating = 3.77f, formattedPrice = "Free", imageUrl = "https://backend.studyround.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcWdCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--02144408fb9e7ff4ca98a8af7ef457d649664e60/image.jpeg"),
+                    )
+                ),
+
+                Category(
+                    1, "Four",
+                    listOf(
+                        Course(1, title = "A course", rating = 3.77f, formattedPrice = "Free", imageUrl = "https://backend.studyround.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcWdCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--02144408fb9e7ff4ca98a8af7ef457d649664e60/image.jpeg"),
+                        Course(1, title = "A course", rating = 3.77f, formattedPrice = "Free", imageUrl = "https://backend.studyround.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcWdCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--02144408fb9e7ff4ca98a8af7ef457d649664e60/image.jpeg"),
+                        Course(1, title = "A course", rating = 3.77f, formattedPrice = "Free", imageUrl = "https://backend.studyround.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcWdCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--02144408fb9e7ff4ca98a8af7ef457d649664e60/image.jpeg"),
+                        Course(1, title = "A course", rating = 3.77f, formattedPrice = "Free", imageUrl = "https://backend.studyround.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcWdCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--02144408fb9e7ff4ca98a8af7ef457d649664e60/image.jpeg"),
+                        Course(1, title = "A course", rating = 3.77f, formattedPrice = "Free", imageUrl = "https://backend.studyround.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcWdCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--02144408fb9e7ff4ca98a8af7ef457d649664e60/image.jpeg"),
+                    )
+                )
+            ))
         }
     }
 
